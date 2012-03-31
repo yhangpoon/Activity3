2bc0dc0d9ee2ff10ddd6a685dd5abc57b623c9c5
sst8696 <sst8696@rit.edu>
2012-03-31 15:56:23 -0400

    Implemented Call method in FileSearch.java

--- src/FileSearch.java
+++ src/FileSearch.java
@@ -1,24 +1,42 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 
 public class FileSearch implements Callable<Found> {
 	
 	private final File currentFile;
+	private final String patternString;
+	private ArrayList<String> list = new ArrayList<String>();
 	
-	public FileSearch(File currentFile){
+	public FileSearch(File currentFile, String patternString){
 		this.currentFile=currentFile;
+		this.patternString=patternString;
 	}
 
 	public Found call() throws Exception{
 		FileReader fstream = new FileReader(currentFile);
 		BufferedReader reader = new BufferedReader(fstream);
+		String currentLine;
+		long lineCount=0;
 		Found result = new Found();
+		Pattern expression = Pattern.compile(patternString);
+		
+		while((currentLine=reader.readLine())!=null){
+			Matcher matcher = expression.matcher(currentLine);
+			if(matcher.matches()){
+				list.add(lineCount + currentLine);
+			}
+			lineCount++;
+		}
+		
+		
+		
 		return result;
 	}
 }
